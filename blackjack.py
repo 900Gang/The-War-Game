2636e18138b10150c2781411b2e0686da0716f2b
900Gang <anandanand6776@gmail.com>
2026-06-18 18:54:01 +0530

    I have added new class content and a bet func

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,9 @@
 suits = ('Hearts', 'Diamonds', 'Clubs', 'Spades')
+
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+
 values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+
 import random
 
 class Card:
@@ -19,11 +22,22 @@ class Deck:
                 creat_card = Card(suit,rank)
                 self.all_cards.append(creat_card)
     
+
+
+    def __str__(self):
+        deck_com=''
+        for card in self.all_cards:
+            deck_comp+='\n'+ card.__str__()
+        return "The deck has: "+deck_com
+
+
+
     def shuffle(self):
         random.shuffle(self.all_cards)
 
     def deal_one(self):
-        return self.all_cards.pop()
+        single_card=self.all_cards.pop()
+        return single_card
     
 class Hand:
     def __init__(self):
@@ -31,17 +45,24 @@ class Hand:
         self.value=0
         self.aces=0
 
-    def add_cards(self,cards):
-        pass
+    def add_cards(self,card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+
+        if card.rank=='Ace':
+            self.aces+=1
 
 
     def adjust_for_ace(self):
-        pass
+        while self.value > 21 and self.aces > 0:
+            self.value-=10
+            self.aces-=1
+        
 
 
 class chips:
-    def __init__(self):
-        self.total = 100
+    def __init__(self,total=100):
+        self.total = total
         self.bet=0
 
     def win_bet(self):
